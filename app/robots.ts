74d77a338c19dc602a449437cdfa3777217f572a
PromptStudio <promptstudio@users.noreply.github.com>
2026-09-03 08:11:24 +0000

    Complete Next.js migration for search tools models and SEO routes

---
+++ app/robots.ts
@@ -0,0 +1,1 @@
+import type{MetadataRoute}from'next';export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/',disallow:['/saved']},sitemap:'https://promptstudio-lemon.vercel.app/sitemap.xml'}}
